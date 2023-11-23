8791acce84558cd1e23ba963be00d169f5ff6019
amv0107 <amv0107@gmail.com>
2023-11-23 23:27:25 +0200

    alpha

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# Анимация при переходе на другой фгагмент
+
+ - enterAnim - Анимация открытии DetailFragment
+ - exitAnim - Анимация закрытия MainFragment
+ - popEnterAnim - Анимация MainFragment при pop action
+ - popExitAnim - Анимация DetailFragment при pop action
+
+## Alpha - Анимация постепенного появления или изчезновения
+ * android:fromAlpha - Начальное смещение непрозрачности, где 0,0 - прозрачный, а 1,0 - непрозрачный.
+ * android:toAlpha - Конечное смещение непрозрачности, где 0,0 - прозрачный, а 1,0 - непрозрачный.
---
+++ app/src/main/java/com/amv0107/anim/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.amv0107.anim
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.amv0107.anim.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding){
+            btnAlpha.setOnClickListener {
+                Navigation.findNavController(it).navigate(R.id.alpha)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
